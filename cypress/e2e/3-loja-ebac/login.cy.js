0efd1c7768fa74e11173c541b35b29932775f723
Sarah Araujo <sarahjanne2008@gmail.com>
2024-09-05 16:13:20 -0300

    arquivo de dados - alteração com massa de dados e fixture

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 ///<reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
@@ -32,5 +33,18 @@ describe('Funcionalidade: Login', () => {
         cy.get('.woocommerce-error > li').should('contain' , 'Erro: A senha fornecida para o e-mail sarahjanne2008@gmail.com está incorreta. Perdeu a senha?')
         cy.get('.woocommerce-error > li').should('exist')
     })
-
+    it('Deve fazer login com sucesso - Usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, sarahjanne2008')
+    });
+    it.only('Deve fazer login com sucesso - Usando fixture', () => {
+       cy.fixture('perfil').then(dados =>{
+        cy.get('#username').type(dados.usuario, {log:false}) //o comando faz com que não sejam apresentados os dados em tela durante teste no test body
+        cy.get('#password').type(dados.senha, {log: false})
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, sarahjanne2008')
+       })
+    });
 })
--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -4,7 +4,7 @@ describe('Produtos', () => {
 
 
         beforeEach(() => {
-            cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+            cy.visit('produtos')
         });
 
     it('Deve selecionar um produto da lista', () => {
